5887a658b93a1dc30a154eff0ef06a091921ea7f
shemil01 <shemilmk1033@gmail.com>
2024-06-08 16:38:16 +0530

    updated

--- src/Components/Pages/Lookbook.js
+++ src/Components/Pages/Lookbook.js
@@ -13,7 +13,7 @@ const Lookbook = () => {
       <NavBar/>
       <div className="col-12 pt-4 main-image pe-5 ps-5 ">
       <h1 style={{color:'black'}}>LOOK BOOK</h1>
-      <img className='img-fluid img-thumbnail'  style={{backgroundPosition:'50%' , backgroundSize:'cover',height:'40rem',width:'1400px'}} src="https://websitedemos.net/recycled-shoe-store-04/wp-content/uploads/sites/983/2021/11/recycled-shoe-store-lookbook-cover-image-4.jpg" alt="" />
+      <img  style={{backgroundPosition:'50%' , backgroundSize:'cover',height:'40rem',width:'1400px'}} src="https://websitedemos.net/recycled-shoe-store-04/wp-content/uploads/sites/983/2021/11/recycled-shoe-store-lookbook-cover-image-4.jpg" alt="" />
      <div>
       <h1>Fall/Winter 2021</h1>
       <p>Elementum donec leo vulputate sit proin suspendisse<br/> malesuada neque proin gravida ut platea vitae duis hac hac <br/>vel id ipsum ultricies ut faucibus ultrices.</p>
@@ -28,7 +28,7 @@ const Lookbook = () => {
             </MDBBtn>
      </div>
      <div className="col-12 pt-4 main-image pe-5 ps-5">
-      <img  className='img-fluid img-thumbnail' style={{backgroundPosition:'50%' , backgroundSize:'cover',height:'40rem',width:'1400px'}} src="https://websitedemos.net/recycled-shoe-store-04/wp-content/uploads/sites/983/2021/11/recycled-shoe-store-lookbook-cover-image-3.jpg" alt="" />
+      <img style={{backgroundPosition:'50%' , backgroundSize:'cover',height:'40rem',width:'1400px'}} src="https://websitedemos.net/recycled-shoe-store-04/wp-content/uploads/sites/983/2021/11/recycled-shoe-store-lookbook-cover-image-3.jpg" alt="" />
      </div>
      <div>
       <h1>Spring/Summer 2021</h1>
@@ -37,7 +37,7 @@ const Lookbook = () => {
 </p>
      </div>
 <div  className="col-12 pt-4 main-image pe-5 ps-5">
-  <img   className='img-fluid img-thumbnail'  style={{backgroundPosition:'50%' , backgroundSize:'cover',height:'40rem',width:'1400px'}}src="https://websitedemos.net/recycled-shoe-store-04/wp-content/uploads/sites/983/2021/11/recycled-shoe-store-lookbook-cover-image-2.jpg" alt="" />
+  <img  style={{backgroundPosition:'50%' , backgroundSize:'cover',height:'40rem',width:'1400px'}}src="https://websitedemos.net/recycled-shoe-store-04/wp-content/uploads/sites/983/2021/11/recycled-shoe-store-lookbook-cover-image-2.jpg" alt="" />
 </div>
 <div>
   <h1>Adventurer Gear</h1>
